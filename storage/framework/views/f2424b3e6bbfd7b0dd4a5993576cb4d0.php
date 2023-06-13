aead044276b98fa025c7cd006441f86fffc0ef82
rraanggaaaa <dwiranggaoktaz@gmail.com>
2023-06-14 03:12:21 +0700

    Initial commit

---
+++ storage/framework/views/f2424b3e6bbfd7b0dd4a5993576cb4d0.php
@@ -0,0 +1,26 @@
+<script>
+        // Mengambil elemen-elemen yang diperlukan
+        const toggleButton = document.getElementById('toggle');
+        const body = document.body;
+
+        // Mengatur tema awal saat halaman dimuat
+        const currentTheme = localStorage.getItem('theme');
+        if (currentTheme === 'dark') {
+            body.classList.add('dark-mode');
+            toggleButton.checked = true;
+        }
+
+        // Mengubah tema saat tombol di klik
+        toggleButton.addEventListener('change', function() {
+            if (this.checked) {
+                body.classList.add('dark-mode');
+                localStorage.setItem('theme', 'dark');
+            } else {
+                body.classList.remove('dark-mode');
+                localStorage.setItem('theme', 'light');
+            }
+        });
+    </script>
+<footer>
+    <p>&copy; 2023 Portofolio Saya. All rights reserved.</p>
+</footer><?php /**PATH C:\xampp\htdocs\portofolio\resources\views/footer.blade.php ENDPATH**/ ?>
